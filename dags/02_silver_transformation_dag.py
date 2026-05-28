be366b740e6326ad6e70477cd94d9c4c9fa9e6c9
Kaíki Pastore <kaikipastorecruz@gmail.com>
2026-05-28 11:47:56 -0300

    feat: implement data quality gates using great expectations

--- dags/02_silver_transformation_dag.py
+++ dags/02_silver_transformation_dag.py
@@ -7,21 +7,28 @@ default_args = {
     'depends_on_past': False,
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 1,
+    'retries': 0, # Definimos 0 retries para falhar rápido se o dado for ruim
     'retry_delay': timedelta(minutes=2),
 }
 
 with DAG(
     '02_silver_transformation',
     default_args=default_args,
-    description='Runs PySpark job to transform raw Bronze data into typed Silver Parquet',
+    description='Validates and transforms raw Bronze data into typed Silver Parquet',
     schedule_interval=timedelta(minutes=10),
     start_date=datetime(2026, 1, 1),
     catchup=False,
-    tags=['silver', 'pyspark', 'transformation'],
+    tags=['silver', 'pyspark', 'data_quality'],
 ) as dag:
+
+    run_quality_checks = BashOperator(
+        task_id='run_data_quality_checks',
+        bash_command='rm -rf /home/airflow/.ivy2 && python /opt/airflow/jobs/quality_checks.py'
+    )
 
     run_silver_spark_job = BashOperator(
         task_id='run_silver_transformation_pyspark',
-        bash_command='rm -rf /home/airflow/.ivy2 && python /opt/airflow/jobs/silver_transformation.py'
+        bash_command='python /opt/airflow/jobs/silver_transformation.py'
     )
+
+    run_quality_checks >> run_silver_spark_job
